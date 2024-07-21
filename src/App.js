bafba54c5b340fea8046e84f954df12489c596a2
Luciano De Pasquale <luchodepasquale@gmail.com>
2024-07-21 23:26:57 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+      <div>
+        <header className='App-header'>
+          <h1>.luchodpdev</h1>
+        
+          <div className='header-cards'>
+            <div className='cards-card'>
+              <h2>experiencia</h2>
+              <p>lorem impsum</p>
+            </div>
+            <div className='cards-card'>
+              <h2>proyectos</h2>
+              <p>lorem impsum</p>
+            </div>
+          </div>
+        </header>
+        <main className='App-main'>
+        <h3>sobre mi</h3>
+        <div className='main-cards'>
+
+        <div className='cards-card'>
+              <h2>front-end</h2>
+              <p>lorem impsum</p>
+            </div>
+            <div className='cards-card'>
+              <h2>productor <br/>musical</h2>
+              <p>lorem impsum</p>
+            </div>
+           
+        </div>
+
+        <h3>conocimientos</h3>
+        <div className='main-cards'>
+
+        <div className='cards-software'>
+              <h2>front-end</h2>
+              <img src='../public/logo192.png' />
+              <img src='../public/logo192.png' />
+              <img src='../public/logo192.png' />
+              <img src='../public/logo192.png' />
+            </div>
+            <div className='cards-software'>
+              <h2>productor <br/>musical</h2>
+              <img src='../public/logo192.png' />
+              <img src='../public/logo192.png' />
+              <img src='../public/logo192.png' />
+              <img src='../public/logo192.png' />
+            </div>
+           
+        </div>
+        </main>
+      </div>
+    )
+  
 }
 
 export default App;
