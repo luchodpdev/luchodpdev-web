25d22863887bb341d14d12aaed45bd8d53424d5f
Luciano De Pasquale <luchodepasquale@gmail.com>
2024-07-22 14:22:53 +0200

    cambios 22 de julio

--- src/App.js
+++ src/App.js
@@ -6,7 +6,19 @@ function App() {
     return (
       <div>
         <header className='App-header'>
-          <h1>.luchodpdev</h1>
+          <nav className='navbar'>
+            
+              <h1>.luchodpdev</h1>
+              <div id='navbaritems'>
+                <a>experiencia</a>
+                <a>proyectos</a>
+                <a>sobre mi</a>
+                <a>contacto</a>
+                <a>portfolio</a></div>
+            
+          </nav>
+          
+          
         
           <div className='header-cards'>
             <div className='cards-card'>
@@ -21,6 +33,25 @@ function App() {
         </header>
         <main className='App-main'>
         <h3>sobre mi</h3>
+
+        <fieldset className='main-fieldset'>
+          <img id='fotoyo'
+          src='./yo.jpg' />
+          <div>
+            <h4>Luciano De Pasquale</h4>
+            <p>
+              lorem impsum lorem impsum
+            lorem impsum lorem impsum lorem impsum
+            lorem impsum lorem impsum lorem impsum
+            lorem impsum lorem impsum lorem impsum
+            lorem impsum lorem impsum lorem impsum
+            lorem impsum lorem impsum lorem impsum
+            lorem impsum lorem impsum lorem impsum
+            lorem impsum lorem impsum lorem impsum
+            </p>
+          </div>
+        </fieldset>
+
         <div className='main-cards'>
 
         <div className='cards-card'>
@@ -39,21 +70,28 @@ function App() {
 
         <div className='cards-software'>
               <h2>front-end</h2>
-              <img src='../public/logo192.png' />
-              <img src='../public/logo192.png' />
-              <img src='../public/logo192.png' />
-              <img src='../public/logo192.png' />
+              <img className='software-logo2' src='./cssjshtmllogo.png' />
+              <img className='software-logo' src='./logonode.jpg' />
+              <img className='software-logo' src='./tslogo.png' />
+              <img className='software-logo' src='./logo192.png' />
+              <img className='software-logo' src='./sasskigi.png' />
+              <img className='software-logo' src='./vuelogo.png' />
             </div>
             <div className='cards-software'>
               <h2>productor <br/>musical</h2>
-              <img src='../public/logo192.png' />
-              <img src='../public/logo192.png' />
-              <img src='../public/logo192.png' />
-              <img src='../public/logo192.png' />
+              <img className='software-logo' src='./logoprotools.webp' />
+              <img className='software-logo' src='./cubase.png' />
+              <img className='software-logo' src='./nuendo.png' />
+              <img className='software-logo' src='./logowaves.jpeg' />
             </div>
            
         </div>
         </main>
+        <footer className='class-footer'>
+          <p>
+            <small className='footer-rights'>Todos los derechos reservados</small>
+            </p>
+        </footer>
       </div>
     )
   
